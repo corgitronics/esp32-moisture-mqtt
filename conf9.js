c566b163f9e841f5a497b3a36ba4a5571bf784d5
corgitronics <corgitronics@users.noreply.github.com>
2022-04-27 21:15:45 -0500

    Create conf9.js
    You must replace the X's with your specific values.

---
+++ conf9.js
@@ -0,0 +1,29 @@
+{
+  "device": {
+    "id": "XXXXXXXXXXXXXX"
+  },
+  "mqtt": {
+    "enable": true,
+    "server": "XXXXXXXXXX.iot.us-west-2.amazonaws.com:8883",
+    "ssl_cert": "XXXXXXXXXXXXX.crt.pem",
+    "ssl_key": "XXXXXXXXXXXXX.key.pem",
+    "ssl_ca_cert": "ca.pem"
+  },
+  "aws": {
+    "thing_name": "XXXXXXXXXX"
+  },
+  "bt": {
+    "enable": false
+  },
+  "wifi": {
+    "ap": {
+      "enable": false
+    },
+    "sta": {
+      "enable": true,
+      "ssid": "XXXXXXXXXXXX",
+      "pass": "XXXXXXXXXXXXXXX"
+    }
+  }
+}
+
